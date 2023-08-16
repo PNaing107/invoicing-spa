5430ab68de7b38683b20734a84cbc68416921eff
Phone Naing <phone.naing.dev@gmail.com>
2023-08-16 20:01:10 +0100

    feat: basic invoice list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
   <TheHeader></TheHeader>
+  <InvoiceList :invoices="invoices"></InvoiceList>
 </template>
 
 <script>
 import TheHeader from './components/UI/TheHeader.vue';
+import InvoiceList from './components/Invoices/InvoiceList.vue';
 
 export default {
   components: {
-    TheHeader
+    TheHeader,
+    InvoiceList
+  },
+  data() {
+    return {
+      invoices: {
+        error: false,
+        data: []
+      },
+    }
+  },
+  created() {
+    fetch('https://invoicing-api.dev.io-academy.uk/invoices')
+      .then(res => res.json())
+      .then(res => {
+        this.invoices.data = res.data;
+        // console.log(res.data);
+      })
+      .catch(err => {
+        this.invoices.error = true;
+        this.invoices.data = err;
+      })
   }
 }
 </script>
---
+++ src/components/Invoices/InvoiceList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-if="invoices.error">The following error has occurred when trying to fetch the invoices:\n {{ invoices.data }}</div>
+    <div v-else v-for="invoice in invoices.data" :key="invoice.id">
+        <div>
+            <p>{{ invoice.invoice_id }}</p>
+            <p>Due {{ invoice.due }}</p>
+            <p>{{ invoice.name }}</p>
+        </div>
+        <div>
+            <p>£{{ invoice.invoice_total }}</p>
+            <div>{{ invoice.status_name }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['invoices'],
+}
+</script>
